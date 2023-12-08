2d7a86c6d10839ff188bca94a272082619156e1a
GonzaloNog <63429016+GonzaloNog@users.noreply.github.com>
2023-12-08 14:26:26 -0300

    update codigo

--- Assets/CharacterControler.cs
+++ Assets/CharacterControler.cs
@@ -1,40 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CharacterControler : MonoBehaviour
 {
-    public float speed;
-    public GameObject fire;
-    private Rigidbody2D rb;
+    public float speed; //controla la velocidad de la nave
+    public float lifePoints = 100.0f; //puntos de vida de nuestra nave
+    public GameObject fire; //sprite de fuego para la nave en movimiento
+    private Rigidbody2D rb; //variable para guardad una referencia al rigidbody2D de nuestro personaje
+    private bool life = true;//variable que determina si nuestra nave sigue viva o fue destruida
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        fire.SetActive(false);
+        rb = GetComponent<Rigidbody2D>();//buscamos dentro de nuestro objeto un componente de tipo "Rigidbody2D" y la guardamos en nuestra variable
+        fire.SetActive(false);//como la nave arranca apagada queremos que ni bien se ejecute el script el fuego este apagado
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");//ingresa la informacion de los input Horizontal entre -1 y 1 (0 si ninguna tencla esta precionada)
+        float moveVertical = Input.GetAxis("Vertical");//ingresa la informacion de los input Vertical entre -1 y 1 (0 si ninguna tencla esta precionada)
 
-        Vector2 movement = new Vector2(moveHorizontal,moveVertical);
+        Vector2 movement = new Vector2(moveHorizontal,moveVertical);//creamos un vector 2 usando las dos funciones creadas anteriormente, para determinar una direccion en un plano 2d 
 
-        rb.velocity = movement * speed;
-        if(rb.velocity.x == 0 && rb.velocity.y == 0)
-            fire.SetActive(false);
+        rb.velocity = movement * speed;//aplicamos una veocidad a nuestro objeto, hacia una direccion fija * la velocidad en nuestra variable speed
+        if(rb.velocity.x == 0 && rb.velocity.y == 0)//este if determina si tenemos que mostrar el sprite del fuego de la nave, para esto comprobamos si se esta moviendo en alguna direccion
+            fire.SetActive(false);//fuego apagado
         else
-            fire.SetActive(true);
+            fire.SetActive(true);//fuego prendido
     }
+
+    public void setLifePoints(float points)//esta funcion va a modificar los puntos de vida de la nave y determinar si fue o no destruida
+    {
+        lifePoints += points;//sumamos los puntos pasados a nuestra variable de vida
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "borde")
+        if(collision.gameObject.tag == "borde")//comprobamos que el tag del objeto contra el que colisionamos, sea un borde del scenario
         {
-            if(this.transform.position.y < 0)
-                this.transform.position = new Vector2(this.transform.position.x, (this.transform.position.y * (-1)) - 1);
+            if(this.transform.position.y < 0)//si la posicion en Y es menor a 0 significa que estamos colicionando con el borde inferior y que queremos aparecer en la aprte superior, si no, se ejecuta lo opuesto
+                this.transform.position = new Vector3(this.transform.position.x, (this.transform.position.y * (-1)) - 1, this.transform.position.z);
             else
-                this.transform.position = new Vector2(this.transform.position.x, (this.transform.position.y * (-1)) + 1);
+                this.transform.position = new Vector3(this.transform.position.x, (this.transform.position.y * (-1)) + 1, this.transform.position.z);
         }
     }
 }
--- Assets/MoveBackground.cs
+++ Assets/MoveBackground.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class MoveBackground : MonoBehaviour
 {
-    public float startPoint;
-    public float endPoint;
-    public float speed;
+    public float startPoint;//Definimos el punto de inicio de nuestro fondo
+    public float endPoint;//definimos el punto de fin de nuestro fondo
+    public float speed;//definimos la velocidad en la que queremos que se mueva el fondo
+
     void Update()
     {
-        this.transform.position = new Vector2(this.transform.position.x - (speed * Time.deltaTime), this.transform.position.y);
-        if (this.transform.position.x < endPoint)
+        this.transform.position = new Vector2(this.transform.position.x - (speed * Time.deltaTime), this.transform.position.y);//movemos en cada frame la imagen en direccion x, se usa TimeDeltaTime, para que se mueva a la misma velocidad siempre indistintamente de la PC
+        if (this.transform.position.x < endPoint)//si su posicion es menor a la posicion de endPoint reseteamos el fondo 
             restart();
     }
 
     void restart()
     {
-        this.transform.position = new Vector2(startPoint, this.transform.position.y);
+        this.transform.position = new Vector2(startPoint, this.transform.position.y);//reseteamos la posicion X de la imagen con el startPoint 
     }
 }
